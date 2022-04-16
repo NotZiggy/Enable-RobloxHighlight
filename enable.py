5e4a0560795e03cd0356c1a9db8bc443f731a918
Ziggy <96440226+NotZiggy@users.noreply.github.com>
2022-04-16 15:21:54 +0200

    Add files via upload

--- enable.py
+++ enable.py
@@ -6,14 +6,15 @@ rblxpath = "C:\Program Files (x86)\Roblox\Versions"
 r = json.loads(urllib.request.urlopen("https://clientsettings.roblox.com/v1/client-version/WindowsPlayer").read())
 rblxpath = rblxpath + "\\" + r["clientVersionUpload"]
 if os.path.exists(rblxpath + "\\ClientSettings"):
-    print("Folder Found!")
+    #print("Folder Found!")
     rblxpath = rblxpath + "\\ClientSettings"
 else:
     os.mkdir(rblxpath + "\\ClientSettings")
-    print("Folder Created!")
+    #print("Folder Created!")
     rblxpath = rblxpath + "\\ClientSettings"
 with open(rblxpath + "\\ClientAppSettings.json", "w") as cas:
     cas.write("{\"FFlagRenderHighlightPass3\": \"True\"}")
     cas.close()
+print("FFlagRenderHighlightPass3: True")
 print("Done!")
 os.system("pause")
